a7f39faaba92f6438f6808c61f997b9dfbe5417a
@marcos.henke <marcos.henke@dponet.com.br>
2024-07-07 22:29:55 -0300

    configurando mailer e verificação de email e recuperação de senha

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -30,7 +30,7 @@ export default function ProfilePage() {
       <h1>Profile</h1>
       <hr />
       <p>Profile page</p>
-      <h2>
+      <h2 className="p-1 rounded bg-green-500">
         {data === "nothing" ? (
           "Não há dados sobre o usuário"
         ) : (
@@ -42,6 +42,12 @@ export default function ProfilePage() {
         className="bg-blue-500 mt-4 hover:bg-blue-700 text-whit font-bold py-2 px-4 rounded"
       >
         Logout
+      </button>
+      <button
+        onClick={getUserDetails}
+        className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Pegar Detalhes do usuário
       </button>
     </div>
   );
---
+++ src/helpers/mailter.ts
@@ -0,0 +1,48 @@
+import nodemailer from "nodemailer";
+import User from "@/models/userModel";
+import bcryptjs from "bcryptjs";
+
+export const sendEmail = async ({ email, emailType, userId }: any) => {
+  try {
+    const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000,
+      });
+    } else if (emailType === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        forgotPasswordToken: hashedToken,
+        forgotPasswordTokenExpiry: Date.now() + 3600000,
+      });
+    }
+
+    const transport = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: process.env.USER_MAILER,
+        pass: process.env.PASSWORD_MAILER,
+      },
+    });
+
+    const mailOptions = {
+      from: "marcos.henke3@gmail.com",
+      to: email,
+      subject:
+        emailType === "VERIFY" ? "Verifique seu e-mail" : "Resete sua senha",
+      html: `<p>Clique <a href="${
+        process.env.domain
+      }/verifyemail?token=${hashedToken}">aqui</a> para ${
+        emailType === "VERIFY" ? "verifique seu email" : "resete sua senha"
+      }</p>`,
+    };
+
+    const mailResponse = await transport.sendMail(mailOptions);
+
+    return mailResponse;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
