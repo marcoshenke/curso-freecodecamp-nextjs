d2ffaff9f15a6d72f8c8206215c23163a8c5adcb
@marcos.henke <marcos.henke@dponet.com.br>
2024-07-08 22:20:42 -0300

    ajuste na rota de login

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -52,7 +52,7 @@ export async function POST(request: NextRequest) {
 
     return response;
   } catch (error: any) {
-    return NextResponse.json({ error: error.message }), { status: 500 };
+    return NextResponse.json({ error: error.message }, { status: 500 });
   } finally {
   }
 }
